909f796a67f17ae4c6685cabdddf5fb775805cf1
Alecks <msae26@hotmail.com>
2023-06-08 05:51:25 -0400

    Updated the js file with task 2.2
    This change updates the js file as exercise 2.2 request.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
-alert('Hello World');
-let favoriteFood = 'Lucuma';
-document.write('My favorite fuit is the: ')
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Igglybuff',
+        height: 6,
+        types: ['normal', 'fairy']
+    },
+    {
+        name: 'Charizard',
+        height: 5,
+        types: ['fire', 'flying']
+    },
+    {
+        name: 'Dragonite',
+        height: 4,
+        types: ['dragon', 'flying']
+    },
+    {
+        name: 'Gastly',
+        height: 3,
+        types: ['ghost', 'posion']
+    }
+];
+
+
