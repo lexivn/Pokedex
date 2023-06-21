22e9dae87a3ca07201d450a8400b077001cfd58e
Alecks <msae26@hotmail.com>
2023-06-20 20:24:35 -0400

    Updated script,js file
    1. Use foreach instead of the for loop to iterate over the Pokemon in your pokemonList array.
    2. Wrap your pokemonList array in an IIFE to avoid accidentally accessing the global state.
    3. The IIFE should return an object with the following public functions assigned as keys:
    getAll: return all items (pokemonRepository.getAll(); should return the pokemonList array)
    add: add a single item to the pokemonList array (calling pokemonRepository.add(item); should add the Pokémon referred to with item to the pokemonList array).
    4. Validating the item added to the pokemonList is an object.

--- js/script.js
+++ js/script.js
@@ -1,44 +1,62 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 7,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Igglybuff',
-        height: 5.6,
-        types: ['normal', 'fairy']
-    },
-    {
-        name: 'Charizard',
-        height: 0.5,
-        types: ['fire', 'flying']
-    },
-    {
-        name: 'Dragonite',
-        height: 2.4,
-        types: ['dragon', 'flying']
-    },
-    {
-        name: 'Gastly',
-        height: 0.3,
-        types: ['ghost', 'posion']
-    }
-];
 
-let pokBigmsg = "";
-let pokname = "";
-let pokheigth = 0;
+pokemonRepository = (function(){
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 7,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Igglybuff',
+            height: 5.6,
+            types: ['normal', 'fairy']
+        },
+        {
+            name: 'Charizard',
+            height: 0.5,
+            types: ['fire', 'flying']
+        },
+        {
+            name: 'Dragonite',
+            height: 2.4,
+            types: ['dragon', 'flying']
+        },
+        {
+            name: 'Gastly',
+            height: 0.3,
+            types: ['ghost', 'posion']
+        }
 
-for (let i = 0; i < pokemonList.length; i++ ) {
-    pokBigmsg = "";
-    pokname = pokemonList[i].name;
-    pokheigth = pokemonList[i].height;
-    
-    if (pokheigth >= 7){
-        pokBigmsg = " - Wow, that's big!";
+    ];
+
+    function add(item) {
+        if(typeof(item) === "object") {
+            if (Object.keys(item) === Object.keys(pokemonList[0]))
+            pokemonList.push(item);
+        }
     }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        getAll: getAll,
+        add: add        
+    };
     
-    document.write(`${pokname} ( height: ${pokheigth}) ${pokBigmsg} <br>`);
-}
+})();
+
+pokemonList = pokemonRepository.getAll();
+
+pokemonList.forEach( pokemon => {
+    let bigPokemon = '';
+    if(pokemon.height >= 7)
+    {
+        bigPokemon = " - Wohm, that's big!";
+    }
+
+    document.write(`${pokemon.name} (height: ${pokemon.height}) ${bigPokemon} <br>`);
+});
+
 
