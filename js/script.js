fdb9a69e72529a95c49e08c108d55b93247cbf93
Alecks <msae26@hotmail.com>
2023-07-04 03:34:04 -0400

    Added <div> for loading message
    This change added the functions showLoadingMessage and hideLoadingMessage for showing up  and hiding the message "Loading Information" when the Pokemon list is pulled from API pokemon

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 // IIFE 
 pokemonRepository = (function () {
- 
+
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function add(item) {
         if (typeof (item) === 'object') {
             Object.keys
-           // if (JSON.stringify(Object.keys(item)) === JSON.stringify(Object.keys(pokemonList[0]))) {
-                pokemonList.push(item);
+            // if (JSON.stringify(Object.keys(item)) === JSON.stringify(Object.keys(pokemonList[0]))) {
+            pokemonList.push(item);
             //}
-                
+
         }
     }
 
@@ -30,19 +30,36 @@ pokemonRepository = (function () {
         listItem.appendChild(button);                                   // Appending the button to the list <li> as a child
         container.appendChild(listItem);                                // Appending the <li> elements to the unordered list <ul> as its child
 
-        button.addEventListener('click', function(){                    // Creating the event handler
-            showDetails(pokemon)});                  
+        button.addEventListener('click', function () {                  // Creating the event handler
+            showDetails(pokemon)
+        });
     }
 
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function () {
             console.log(pokemon);
-        });        
+        });
+    }
+
+    function showLoadingMessage() {
+        let showLoadingMessage = document.querySelector('.loading-message');
+        let p = document.createElement('p');
+        p.innerText = 'Loading information ...';
+        showLoadingMessage.appendChild(p);
+    }
+
+    function hideLoadingMessage() {
+        let removeLoadingMessage = document.querySelector('p');
+        removeLoadingMessage.parentElement.removeChild(removeLoadingMessage);
     }
 
     //Fetch return a promise which is passed to first then function
     function loadList() {
-        return fetch(apiUrl).then(function (response) {
+
+        showLoadingMessage();                                           // Show loading message
+
+        return fetch(apiUrl).then(function (response) {                 // Load data from external resource. 
+            setTimeout(hideLoadingMessage, 500);
             return response.json();
         }).then(function (json) {
             json.results.forEach(function (item) {
@@ -59,9 +76,13 @@ pokemonRepository = (function () {
 
     //DetailsUrl property to load the detailed data for a given Pokémon
     function loadDetails(item) {
+        
+        showLoadingMessage();                                           // Show loading message
+
         let url = item.detailsUrl;
         return fetch(url).then(function (response) {
-            return response.json();     // Taking the json property from the objec(response)
+            setTimeout(hideLoadingMessage, 500);
+            return response.json();                                     // Taking the json property from the objec(response)
         }).then(function (details) {
             // Now we add the details to the item
             item.imageUrl = details.sprites.front_default;
@@ -78,7 +99,7 @@ pokemonRepository = (function () {
         addListItem: addListItem,
         loadList: loadList,
         loadDetails: loadDetails
-        
+
     };
 
 })();
@@ -87,12 +108,8 @@ pokemonRepository = (function () {
 pokemonRepository.add({ name: 'Pikachu', height: 0.3, types: '[electric]' });   // Fix it
 
 // Get all the list of Pokemon to create a button with the name of each one
-// pokemonRepository.getAll().forEach(function(pokemon){
-//     pokemonRepository.addListItem(pokemon);
-// })
-
 pokemonRepository.loadList().then(function (pokemon) {
-    pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.getAll().forEach(function (pokemon) {
         pokemonRepository.addListItem(pokemon);
     });
 });
