e7df0acd05c772cf105f2bcfdddcbce577d9fc7f
Alecks <msae26@hotmail.com>
2023-07-13 05:35:41 -0400

    Added the showModal function
    This change adds the modalShow function which shows up the Pokemon's name, height, and image.
    Furthermore adds the events to hide the modal when clicking the close modal button or clicking outside of he modal.

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ pokemonRepository = (function () {
 
     function add(item) {
         if (typeof (item) === 'object') {
-            Object.keys
+            //Object.keys
             // if (JSON.stringify(Object.keys(item)) === JSON.stringify(Object.keys(pokemonList[0]))) {
             pokemonList.push(item);
             //}
@@ -36,30 +36,75 @@ pokemonRepository = (function () {
     }
 
     function showDetails(pokemon) {
-        loadDetails(pokemon).then(function () {
+         loadDetails(pokemon).then(function () {             
+            // Here start the function to make Modal visible          
+            showModal(pokemon);
             console.log(pokemon);
         });
+     
     }
 
-    function showLoadingMessage() {
-        let showLoadingMessage = document.querySelector('.loading-message');
-        let p = document.createElement('p');
-        p.innerText = 'Loading information ...';
-        showLoadingMessage.appendChild(p);
+    function showModal(item) {
+        let modalContainer = document.querySelector('#modal-container');
+        
+        // Clear all the existing modal content
+        modalContainer.innerHTML = '';
+
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+
+        // Add the new modal content
+        let closeButtonElement = document.createElement('button');
+        closeButtonElement.classList.add('modal-close');
+        closeButtonElement.innerText = 'Close';
+        closeButtonElement.addEventListener('click', hideModal);
+
+        let pokemonName = document.createElement('h1');
+        pokemonName.innerHTML = item.name;
+
+        let pokemonHeight = document.createElement('p');
+        pokemonHeight.innerHTML = ('Height: ' + item.height);
+
+        let pokemonImage = document.createElement('img');
+        pokemonImage.setAttribute('src', item.imageUrl);
+
+        modal.appendChild(closeButtonElement);
+        modal.appendChild(pokemonName);
+        modal.appendChild(pokemonHeight);
+        modal.appendChild(pokemonImage);
+        modalContainer.appendChild(modal);
+
+        modalContainer.classList.add('is-visible');
+
+        modalContainer.addEventListener('click', (e) => {
+            let target = e.target;
+            if (target === modalContainer) {
+                hideModal();
+            }
+        });
+    }
+  
+
+    function hideModal(){
+        let modalContainer = document.querySelector('#modal-container');
+        modalContainer.classList.remove('is-visible');
     }
 
-    function hideLoadingMessage() {
-        let removeLoadingMessage = document.querySelector('p');
-        removeLoadingMessage.parentElement.removeChild(removeLoadingMessage);
-    }
+    // Hide the modal when click outside the modal
+    window.addEventListener('keydown', (e)=> {
+        modalContainer = document.querySelector('#modal-container');
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+            hideModal();
+        }
+    });
 
     //Fetch return a promise which is passed to first then function
     function loadList() {
-
-        showLoadingMessage();                                           // Show loading message
+        
+        //showLoadingMessage();                                           // Show loading message
 
         return fetch(apiUrl).then(function (response) {                 // Load data from external resource. 
-            setTimeout(hideLoadingMessage, 500);
+           // setTimeout(hideLoadingMessage, 500);
             return response.json();
         }).then(function (json) {
             json.results.forEach(function (item) {
@@ -77,11 +122,11 @@ pokemonRepository = (function () {
     //DetailsUrl property to load the detailed data for a given Pokémon
     function loadDetails(item) {
         
-        showLoadingMessage();                                           // Show loading message
+        //showLoadingMessage();                                           // Show loading message
 
         let url = item.detailsUrl;
         return fetch(url).then(function (response) {
-            setTimeout(hideLoadingMessage, 500);
+           // setTimeout(hideLoadingMessage, 500);                        // Set a timeout to hide the "Loading message"
             return response.json();                                     // Taking the json property from the objec(response)
         }).then(function (details) {
             // Now we add the details to the item
@@ -105,7 +150,7 @@ pokemonRepository = (function () {
 })();
 
 // This function just add 'Pikachu' manually
-pokemonRepository.add({ name: 'Pikachu', height: 0.3, types: '[electric]' });   // Fix it
+//pokemonRepository.add({ name: 'Pikachu', height: 0.3, types: '[electric]' });   // Fix it
 
 // Get all the list of Pokemon to create a button with the name of each one
 pokemonRepository.loadList().then(function (pokemon) {
